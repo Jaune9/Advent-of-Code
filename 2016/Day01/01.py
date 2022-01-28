8b07ae9bb2c2bf16e683b7f4fd49b246feb0ff16
Jaune9 <prinnydad@hotmail.fr>
2022-01-28 22:45:08 +0100

    début 2016

---
+++ 2016/Day01/01.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import math
+
+if __name__ == "__main__":
+
+    # parsing
+    trajectory_string = Path("input.txt").read_text()
+    trajectory_list = trajectory_string.split(", ")
+
+    north_from_start = 0
+    west_from_start = 0
+
+    previous_trajectory = 'N'
+    possible_trajectories = ['N', 'E', 'S', 'W']
+    for trajectory in trajectory_list:
+
+        current_trajectory = ''
+        # determining trajectory
+        if 'L' in trajectory:
+            current_trajectory = possible_trajectories[possible_trajectories.index(previous_trajectory) - 1]
+        elif 'R' in trajectory:
+            if possible_trajectories.index(previous_trajectory) == len(possible_trajectories) - 1:
+                current_trajectory = possible_trajectories[0]
+            else:
+                current_trajectory = possible_trajectories[possible_trajectories.index(previous_trajectory) + 1]
+
+        # parsing
+        move_count = int(trajectory[1:])
+
+        # actual operations
+        if current_trajectory == possible_trajectories[0]:
+            north_from_start += move_count
+        elif current_trajectory == possible_trajectories[1]:
+            west_from_start -= move_count
+        elif current_trajectory == possible_trajectories[2]:
+            north_from_start -= move_count
+        elif current_trajectory == possible_trajectories[3]:
+            west_from_start += move_count
+
+        previous_trajectory = current_trajectory
+    print(f"Distance from start is {north_from_start} North, {west_from_start} West == {abs(north_from_start) + abs(west_from_start)}")
