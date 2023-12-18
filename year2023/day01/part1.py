c4299e00da7e872a1d4568dfdac6606dd17b68f2
Jaune9 <47558505+Jaune9@users.noreply.github.com>
2023-12-18 16:24:56 +0100

    Update part1.py
    refacto a lambda into a func for clarity

--- year2023/day01/part1.py
+++ year2023/day01/part1.py
@@ -1,11 +1,13 @@
+def get_first_number_in_str(the_string: str) -> str:
+    for letter in the_string:
+        if '0' <= letter <= '9': return letter
+
 def get_sum_of_first_and_last_number_of_each_line(code: str) -> int:
     lines = code.split('\n')
     result = 0
     for line in lines:
         current = ''
-        r_line = reversed(line)
-        get_first_number = lambda word: [letter for letter in word if '0' <= letter <= '9'][0]
-        current += get_first_number(line) + get_first_number(r_line)
+        current += get_first_number_in_str(line) + get_first_number_in_str(reversed(line))
         if current:
             result += int(current)
     return result
