72cce2573c2b9a5510ada6260e040c8c52aa4c9d
Jaune9 <47558505+Jaune9@users.noreply.github.com>
2023-12-18 16:18:55 +0100

    Update part1.py
    Replace 2 for loop by a lambda with a list comprehension

--- year2023/day01/part1.py
+++ year2023/day01/part1.py
@@ -3,14 +3,9 @@ def get_sum_of_first_and_last_number_of_each_line(code: str) -> int:
     result = 0
     for line in lines:
         current = ''
-        for letter in line:
-            if '0' <= letter <= '9':
-                current += letter
-                break
-        for letter in reversed(line):
-            if '0' <= letter <= '9':
-                current += letter
-                break
+        r_line = reversed(line)
+        get_first_number = lambda word: [letter for letter in word if '0' <= letter <= '9'][0]
+        current += get_first_number(line) + get_first_number(r_line)
         if current:
             result += int(current)
     return result
