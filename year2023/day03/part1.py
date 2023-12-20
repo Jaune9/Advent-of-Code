8363e3b5c4b9d0149d1e75c9c675c7fcd8990f3a
Jaune9 <47558505+Jaune9@users.noreply.github.com>
2023-12-20 14:25:54 +0100

    Update part1.py - one more step :)

--- year2023/day03/part1.py
+++ year2023/day03/part1.py
@@ -1,4 +1,4 @@
-def get_adjacent_or_diagonal_content(x, y, row_count, line_length):
+def get_adjacent_or_diagonal_cells(x, y, row_count, line_length):
     near_elements = []
     is_first_row = x == 0
     is_last_row = x == row_count - 1
@@ -7,7 +7,8 @@ def get_adjacent_or_diagonal_content(x, y, row_count, line_length):
 
     # tenter un match ?
 
-    if not is_first_row and not is_first_in_line:      # above and before
+    if not is_first_row and not is_first_in_line:
+        # above and before
         near_elements.append((x - 1, y - 1))
     if not is_first_row:
         # above
@@ -34,9 +35,19 @@ def get_adjacent_or_diagonal_content(x, y, row_count, line_length):
     return near_elements
 
 
+def get_adjacent_content(matrix, cell_x, cell_y):
+    neighbours = get_adjacent_or_diagonal_cells(cell_x, cell_y, len(matrix[0]), len(matrix))
+    neighbours_content = []
+    for neighbour in neighbours:
+        neighbours_content.append(matrix[cell_x+neighbour[0]][cell_y+neighbour[1]])
+    return neighbours_content
+
+
 if __name__ == '__main__':
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
-    print(get_adjacent_or_diagonal_content(1, 1, 3, 3))
-    print(get_adjacent_or_diagonal_content(0, 0, 3, 3))
+    print(get_adjacent_or_diagonal_cells(1, 1, 3, 3))
+    print(get_adjacent_or_diagonal_cells(0, 0, 3, 3))
+    print(get_adjacent_content(matrix, 0, 0))
+
